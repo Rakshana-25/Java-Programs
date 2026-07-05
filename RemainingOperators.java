5c6235f7ed601cf7ce732cc8b186763aeb51edbd
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:38:44 +0530

    practiced using different kind of operators

---
+++ RemainingOperators.java
@@ -0,0 +1,21 @@
+public class RemainingOperators {
+    public static void main (String[] args){
+        int x=25;
+        int y=19;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y); //Arithmetic operation
+        String str="Hello!"; //String concatenation
+        str= str + " " + "Rakshana";
+        System.out.println(str);
+        System.out.println(x>y && x!=y); //Logical operators
+        System.out.println(x==y || x!=y);
+        System.out.println(x!=y);
+        System.out.println(x>y); //Relational operator
+        System.out.println(x<y);
+        System.out.println(x!=y);
+    }
+}
+
