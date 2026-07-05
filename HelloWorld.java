efe4cc59d609747d3991aedc974ac8cba996f368
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:21:48 +0530

    Change output to 'Hello World!' in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        final int num=11;
-        System.out.println(num);
+        System.out.println("Hello World!");
     }
 }
