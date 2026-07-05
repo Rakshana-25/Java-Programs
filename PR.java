524bcc53723f0847c33026bae2e895c287ddb160
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:58:36 +0530

    Function/Methods - Parameter Return program

---
+++ PR.java
@@ -0,0 +1,9 @@
+public class PR{
+    static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+        int sum=add(2,3);
+        System.out.println(sum);
+    }
+}
