5ebfd27a57b42310a593f54ce765ae30402e6fb5
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:58:14 +0530

    Function/Methods - Parameter No return program

---
+++ PNr.java
@@ -0,0 +1,8 @@
+public class PNr{
+    static void add(int a,int b){
+        System.out.print("The sum is"+ " " + (a+b));
+    }
+    public static void main(String[] args){
+        add(10,29);
+    }
+}
