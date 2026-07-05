09db49c21a014b85bf16d1bb193c1a6cd50cecc9
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:36:55 +0530

    practiced data types program

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args){
+        int a = 10;
+        float b = 25.32f;
+        char myChar = 'm';
+        double d = 3.14;
+        boolean bool = true;
+        short num = 350;
+        System.out.println("Integer:" + a);
+        System.out.println("Float:" + b);
+        System.out.println("Character:" + myChar);
+        System.out.println("Double:" + d);
+        System.out.println("Boolean value:" + bool);
+        System.out.println("Short:" + num);
+    }
+}
