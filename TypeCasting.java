7d700ffc838ed32e4d8d0b54e73fc745d21b7e7a
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:48:47 +0530

    coverting one type od data type to another

---
+++ TypeCasting.java
@@ -0,0 +1,16 @@
+public class TypeCasting {
+    public static void main(String[] args){
+        double a = 10d;
+        int b = (int) a;
+        System.out.println("double value: " +a);
+        System.out.println("Integer: "+b);
+        char k = 'A';
+        int val = k;
+        System.out.println("Character: " +k);
+        System.out.println("Integer value: " +val);
+        int num = 100;
+        char ch = (char) num;
+        System.out.println("Integer value: "+num);
+        System.out.println("Charcter value: "+ch);
+    }
+}
