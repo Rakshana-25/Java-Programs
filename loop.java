90a782d155b6fd5d34b9f9dc1d0001f293f8785b
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:53:04 +0530

    looping of numbers

---
+++ loop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class loop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        for(int i=0;i<=num;i++){
+            System.out.println(i);
+        }
+
+    }
+}
