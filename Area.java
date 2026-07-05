ebe1da385c2a94999c1c34dc609107839a45a3e4
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:26:00 +0530

    added calculating the area of circle program

---
+++ Area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Area{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle:");
+        double r = sc.nextDouble();
+        double area = Math.PI*(r*r);
+
+        System.out.println("Area od the circle is "+ area +" square metres");
+    }
+}
