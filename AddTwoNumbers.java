3555a22d1d3e8d25cf3cd36fcc7ea4bfac4924e3
Rakshana-25 <rakshanarohii2006@gmail.com>
2026-07-05 12:24:32 +0530

    Added Add two numbers program

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwoNumbers{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int x = sc.nextInt();
+    System.out.println("Enter the next number:");
+    int y = sc.nextInt();
+
+    System.out.println("The sum of the numbers is " + (x+y));
+
+    }
+}
